6486be78e952ef669aca725eb960710639cf7acd
janphilippfranken <janphilipp.franken@gmail.com>
2024-06-22 13:31:56 -0700

    updates

--- experiments/v1/scripts/expected_info_gain.sh
+++ experiments/v1/scripts/expected_info_gain.sh
@@ -7,14 +7,14 @@
 #SBATCH --mem=64GB                       
 #SBATCH --cpus-per-task=16               
 #SBATCH --time=12:00:00                    
-#SBATCH --output=eig_0_2500_1_9.out         
-#SBATCH --error=eig_0_2500_1_9.err          
+#SBATCH --output=eig_0_100_2_3.out         
+#SBATCH --error=eig_0_100_2_3.err          
 #SBATCH --begin=now+0hours 
 
 seed=0
 prompt_start=0
 prompt_end=100
-n_users_per_prompt=5
+n_users_per_prompt=2
 
 source /scr/jphilipp/miniconda3/etc/profile.d/conda.sh
 conda activate stargate
@@ -24,5 +24,6 @@ cd ~/research_projects/star-gate-human-eval/experiments/v1
 python expected_info_gain.py \
     prompt_start=$prompt_start \
     prompt_end=$prompt_end \
-    conversations=data/conversations/hai_start_${prompt_start}_end_${prompt_end}_n_user_${n_users_per_prompt}_seed_${seed}_9_shots.json \
-    save_file=data/expected_info_gain/hai_start_${prompt_start}_end_${prompt_end}_n_user_${n_users_per_prompt}_seed_${seed}_9_shots.json
+    n_users_per_prompt=$n_users_per_prompt \
+    conversations=data/conversations/hai_start_${prompt_start}_end_${prompt_end}_n_user_${n_users_per_prompt}_seed_${seed}_3_shots.json \
+    save_file=data/expected_info_gain/hai_start_${prompt_start}_end_${prompt_end}_n_user_${n_users_per_prompt}_seed_${seed}_3_shots.json
