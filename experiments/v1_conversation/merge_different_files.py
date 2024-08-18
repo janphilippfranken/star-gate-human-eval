77f5c730ec6a7ded33e12e1d9a064b677b23ced2
janphilippfranken <janphilipp.franken@gmail.com>
2024-08-17 22:16:16 -0700

    updates

--- experiments/v1_conversation/merge_different_files.py
+++ experiments/v1_conversation/merge_different_files.py
@@ -46,13 +46,55 @@ if __name__ == '__main__':
     # ]
     # output_path = 'data/conversations/conv_10k-2_user.json'
     # combine_gold_responses(file_paths, output_path)
-    """ info gain 10k 1 user """
+    # """ info gain 10k 1 user """
+    # file_paths = [
+    #     'data/expected_info_gain/logprobs_250_5000.json',
+    #     'data/expected_info_gain/logprobs_5000_10000.json'
+    # ]
+    # output_path = 'data/expected_info_gain/logprobs_10k.json'
+    # combine_gold_responses(file_paths, output_path)
+    
+    # """ info gain 10k 2 users """
+    # file_paths = [
+    #     'data/expected_info_gain/eig_2_user_250_1000.json',
+    #     'data/expected_info_gain/eig_2_user_1000_2000.json',
+    #     'data/expected_info_gain/eig_2_user_2000_3000.json',
+    #     'data/expected_info_gain/eig_2_user_3000_5000.json',
+    #     'data/expected_info_gain/eig_2_user_5000_7500.json',
+    #     'data/expected_info_gain/eig_2_user_7500_10000.json'
+    # ]
+    # output_path = 'data/expected_info_gain/eig_10k_2_user.json'
+    # combine_gold_responses(file_paths, output_path)
+
+    """ conv 10k 5 users """
     file_paths = [
-        'data/expected_info_gain/logprobs_250_5000.json',
-        'data/expected_info_gain/logprobs_5000_10000.json'
+        'data/conversations/5_user-250_1000_conv.json',
+        'data/conversations/5_user-1000_2000_conv.json',
+        'data/conversations/5_user-2000_3000_conv.json',
+        'data/conversations/5_user-3000_4000_conv.json',
+        'data/conversations/5_user-4000_5000_conv.json',
+        'data/conversations/5_user-5000_6000_conv.json',
+        'data/conversations/5_user-6000_7000_conv.json',
+        'data/conversations/5_user-7000_8000_conv.json',
+        'data/conversations/5_user-8000_9000_conv.json',
+        'data/conversations/5_user-9000_10000_conv.json'
     ]
-    output_path = 'data/expected_info_gain/logprobs_10k.json'
+    output_path = 'data/conversations/conv_10k-5_user.json'
     combine_gold_responses(file_paths, output_path)
-    
-    
+
+    """ info gain 10k 5 users """
+    file_paths = [
+        'data/expected_info_gain/eig_5_user_250_1000.json',
+        'data/expected_info_gain/eig_5_user_1000_2000.json',
+        'data/expected_info_gain/eig_5_user_2000_3000.json',
+        'data/expected_info_gain/eig_5_user_3000_4000.json',
+        'data/expected_info_gain/eig_5_user_4000_5000.json',
+        'data/expected_info_gain/eig_5_user_5000_6000.json',
+        'data/expected_info_gain/eig_5_user_6000_7000.json',
+        'data/expected_info_gain/eig_5_user_7000_8000.json',
+        'data/expected_info_gain/eig_5_user_8000_9000.json',
+        'data/expected_info_gain/eig_5_user_9000_10000.json'
+    ]
+    output_path = 'data/expected_info_gain/eig_10k_5_user.json'
+    combine_gold_responses(file_paths, output_path)
     
