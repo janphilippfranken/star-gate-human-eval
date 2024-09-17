ceec7048bf15fc828f5bc952e6a01fdaf13b9bdb
janphilippfranken <janphilipp.franken@gmail.com>
2024-09-17 09:34:44 -0700

    cleanup

--- experiments/v1_conversation/test_model.py
+++ experiments/v1_conversation/test_model.py
@@ -1,13 +1,23 @@
-from vllm_inference_model import VLLMInferenceModel
+from stargate.vllm_inference_model import VLLMInferenceModel
+
+N_GPUS_YOU_ARE_USING = 1
 
 model = VLLMInferenceModel(
-    model="meta-llama/Meta-Llama-3-8B-Instruct",
-    download_dir="/scr/YOUR_DOWNLOAD_PATH",
+    model="meta-llama/Meta-Llama-3.1-8B-Instruct",
+    download_dir="/scr/jphilipp/stargate/pretrained_models/Meta-Llama-3.1-8B-Instruct",
     dtype="auto",
     tensor_parallel_size=N_GPUS_YOU_ARE_USING,
 )
+from datasets import load_dataset
 
-batch_responses = model.batch_prompt(
+data = load_dataset(
+    "HannahRoseKirk/prism-alignment",
+    cache_dir="./data/v2/prism-alignment"
+)
+# HannahRoseKirk/prism-alignment
+
+breakpoint()
+batch_response = model.batch_prompt(
     prompts=batch_prompts,
     max_new_tokens=2048,
     num_return_sequences=1, 
@@ -15,3 +25,12 @@ batch_responses = model.batch_prompt(
     temperature=0.0,
 )
 
+def prompt_model(prompt):
+    
+    return model.batch_prompt(
+        prompts=prompt,
+        max_new_tokens=2048,
+        num_return_sequences=1, 
+        best_of=1,
+        temperature=0.0,
+    )[0]
